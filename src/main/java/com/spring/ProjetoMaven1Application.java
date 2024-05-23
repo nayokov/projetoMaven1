0d974bb0dcd9cc071e4109ec1fb254563de069bd
Nayan <nayandf@gmail.com>
2024-05-23 15:23:03 -0300

    versão inicial

---
+++ src/main/java/com/spring/ProjetoMaven1Application.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMaven1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMaven1Application.class, args);
+	}
+
+}
